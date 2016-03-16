86ffdd64025903355f36a1d85f6f2871442c61ef
Matthew Dove <matthew.dove@blackcombsoftware.com>
2016-03-16 22:17:07 +1100

    Added tests for the later, and response containers.

---
+++ ContainerExpressions/Tests.ContainerExpressions/Containers/LaterTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MonadicExpressions.Containers;
+using System;
+
+namespace Tests.MonadicExpressions.Containters
+{
+    [TestClass]
+    public class LaterTests
+    {
+        [TestMethod]
+        public void GeneratingFunction_Invoked_OnlyOnce()
+        {
+            var expectedTimesInvoked = 1;
+            var timesGeneratingFunctionInvoked = 0;
+            var later = Later.Create(() => { timesGeneratingFunctionInvoked++; return 42; });
+
+            var attemptOne = later.Value; // Value is initialized here.
+            var attemptTwo = later.Value; // Value is simply returned here (without calling the generating function again).
+
+            Assert.AreEqual(expectedTimesInvoked, timesGeneratingFunctionInvoked);
+            Assert.AreEqual(attemptOne, attemptTwo); // Not a necessary condition to test the numner of times invoked, but I don't want the second attempt optimised away, as that would force the test to always pass.
+        }
+
+        [TestMethod]
+        public void SameValue_Is_AlwaysReturned()
+        {
+            var later = Later.Create(() => Guid.NewGuid());
+
+            var attemptOne = later.Value;
+            var attemptTwo = later.Value;
+
+            Assert.AreEqual(attemptOne, attemptTwo); // If guids are different, we have a problem.
+        }
+
+        [TestMethod]
+        public void Later_Converts_ToT()
+        {
+            var returnValue = 42;
+            var later = Later.Create(() => returnValue);
+
+            int value = later; // Implicit conversion to T.
+
+            Assert.AreEqual(returnValue, value);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullGeneratingFunction_ThrowsError()
+        {
+            Later.Create<object>(null);
+        }
+    }
+}
---
+++ ContainerExpressions/Tests.ContainerExpressions/Containers/ResponseTests.cs
@@ -0,0 +1,198 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MonadicExpressions.Containers;
+using System;
+
+namespace Tests.MonadicExpressions.Containters
+{
+    [TestClass]
+    public class ResponseTests
+    {
+        #region Response
+
+        [TestMethod]
+        public void Response_HasValue_IsValid()
+        {
+            var response = Response.Create(10);
+
+            Assert.IsTrue(response);
+        }
+
+        [TestMethod]
+        public void Response_HasNoValue_IsNotValid()
+        {
+            var response = new Response<int>();
+
+            Assert.IsFalse(response);
+        }
+
+        [TestMethod]
+        public void Response_ToString_ReturnsUnderlyingValueToString()
+        {
+            var value = 10;
+            var response = new Response<int>(value);
+
+            Assert.AreEqual(value.ToString(), response.ToString());
+        }
+
+        [TestMethod]
+        public void Response_Convert_ToT()
+        {
+            var value = 10;
+            var response = new Response<int>(value);
+
+            int result = response;
+
+            Assert.AreEqual(value, result);
+        }
+
+        [TestMethod]
+        public void Response_Convert_ToBool()
+        {
+            var response = new Response<int>(10);
+
+            bool value = response;
+
+            Assert.AreEqual(value, response.IsValid);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Response_GetValueWithInValidState_Error()
+        {
+            var response = new Response<int>();
+
+            int value = response;
+        }
+
+        [TestMethod]
+        public void Response_GetValueOrDefaultWithInValidState_ReturnsDefault()
+        {
+            var defaultValue = 42;
+            var response = new Response<int>();
+
+            int value = response.GetValueOrDefault(defaultValue);
+
+            Assert.AreEqual(defaultValue, value);
+        }
+
+        [TestMethod]
+        public void Response_GetValueOrDefaultWithValidState_ReturnsUnderlyingValue()
+        {
+            var result = 42;
+            var response = new Response<int>(result);
+
+            int value = response.GetValueOrDefault(result * -1);
+
+            Assert.AreEqual(result, value);
+        }
+
+        [TestMethod]
+        public void Response_WithValue_IsValid()
+        {
+            var responseWithNoValue = new Response<int>();
+            var responseWithValue = new Response<int>(42);
+
+            responseWithNoValue = responseWithNoValue.WithValue(42);
+            responseWithValue = responseWithValue.WithValue(42);
+
+            Assert.IsTrue(responseWithNoValue);
+            Assert.IsTrue(responseWithValue);
+        }
+
+        [TestMethod]
+        public void Response_WithNoValue_IsNotValid()
+        {
+            var responseWithValue = new Response<int>(42);
+            var responseWithNoValue = new Response<int>();
+
+            responseWithValue = responseWithValue.WithNoValue();
+            responseWithNoValue = responseWithNoValue.WithNoValue();
+
+            Assert.IsFalse(responseWithValue);
+            Assert.IsFalse(responseWithNoValue);
+        }
+
+        #endregion
+
+        #region BaseResponse
+
+        [TestMethod]
+        public void Response_HasValue_IValid()
+        {
+            var response = new Response(true);
+
+            Assert.IsTrue(response);
+        }
+
+        [TestMethod]
+        public void BaseResponse_False_IsNotValid()
+        {
+            var response = new Response(false);
+
+            Assert.IsFalse(response);
+        }
+
+        [TestMethod]
+        public void BaseResponse_ToString_ReturnsStringValueOfIsValid()
+        {
+            var isValid = true;
+            var response = new Response(isValid);
+
+            Assert.AreEqual(isValid.ToString(), response.ToString());
+        }
+
+        [TestMethod]
+        public void BaseResponse_AsValid_IsValid()
+        {
+            var responseFalse = new Response();
+            var responseTrue = new Response(true);
+
+            responseFalse = responseFalse.AsValid();
+            responseTrue = responseTrue.AsValid();
+
+            Assert.IsTrue(responseFalse);
+            Assert.IsTrue(responseTrue);
+        }
+
+        [TestMethod]
+        public void BaseResponse_AsInvalid_IsNotValid()
+        {
+            var responseTrue = new Response(true);
+            var responseFalse = new Response();
+
+            responseTrue = responseTrue.AsInvalid();
+            responseFalse = responseFalse.AsInvalid();
+
+            Assert.IsFalse(responseTrue);
+            Assert.IsFalse(responseFalse);
+        }
+
+        [TestMethod]
+        public void BaseResponse_Create_InvalidResponseT()
+        {
+            var response = Response.Create<int>();
+
+            Assert.IsFalse(response);
+        }
+
+        [TestMethod]
+        public void BaseResponse_Create_ValidResponseT()
+        {
+            var response = Response.Create(42);
+
+            Assert.IsTrue(response);
+        }
+
+        [TestMethod]
+        public void BaseResponse_Convert_ToBool()
+        {
+            var response = new Response();
+
+            bool value = response;
+
+            Assert.AreEqual(value, response.IsValid);
+        }
+
+        #endregion
+    }
+}
