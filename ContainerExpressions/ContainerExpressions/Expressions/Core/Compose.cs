6d4acf1356f6fe3e8ab034af44617b730678650d
Matthew Dove <matthew.dove@blackcombsoftware.com>
2016-03-23 23:00:38 +1100

    Added the first two Compose expressions.

--- ContainerExpressions/ContainerExpressions/Expressions/Core/Compose.cs
+++ ContainerExpressions/ContainerExpressions/Expressions/Core/Compose.cs
@@ -5,9 +5,19 @@ namespace ContainerExpressions.Expressions.Core
 {
     internal static class Compose
     {
-        public static Response<T> Evaluate<T>(Func<Response<T>> func)
+        public static Response<T> Evaluate<T>(Func<Response<T>> func) => func();
+
+        public static Response<TResult> Evaluate<T1, TResult>(Func<Response<T1>> func1, Func<T1, Response<TResult>> funcResult)
         {
-            return func();
+            var response = new Response<TResult>();
+
+            var result = func1();
+            if (result)
+            {
+                response = response.WithValue(funcResult(result));
+            }
+
+            return response;
         }
     }
 }
--- ContainerExpressions/ContainerExpressions/Expressions/Expression.cs
+++ ContainerExpressions/ContainerExpressions/Expressions/Expression.cs
@@ -6,5 +6,6 @@ namespace ContainerExpressions.Expressions
     public static class Expression
     {
         public static Response<T> Compose<T>(Func<Response<T>> func) => Core.Compose.Evaluate(func);
+        public static Response<TResult> Compose<T1, TResult>(Func<Response<T1>> func1, Func<T1, Response<TResult>> funcResult) => Core.Compose.Evaluate(func1, funcResult);
     }
 }
