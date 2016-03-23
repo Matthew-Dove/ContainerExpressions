d0d4d071a2a3ecd141d1a9e221a425df3c74c3df
Matthew Dove <matthew.dove@blackcombsoftware.com>
2016-03-23 23:13:21 +1100

    Added up to T3 for the compose expression.

--- ContainerExpressions/ContainerExpressions/Expressions/Core/Compose.cs
+++ ContainerExpressions/ContainerExpressions/Expressions/Core/Compose.cs
@@ -19,5 +19,43 @@ namespace ContainerExpressions.Expressions.Core
 
             return response;
         }
+
+        public static Response<TResult> Evaluate<T1, T2, TResult>(Func<Response<T1>> func1, Func<T1, Response<T2>> func2, Func<T2, Response<TResult>> funcResult)
+        {
+            var response = new Response<TResult>();
+
+            var result1 = func1();
+            if (result1)
+            {
+                var result2 = func2(result1);
+                if (result2)
+                {
+                    response = response.WithValue(funcResult(result2));
+                }
+            }
+
+            return response;
+        }
+
+        public static Response<TResult> Evaluate<T1, T2, T3, TResult>(Func<Response<T1>> func1, Func<T1, Response<T2>> func2, Func<T2, Response<T3>> func3, Func<T3, Response<TResult>> funcResult)
+        {
+            var response = new Response<TResult>();
+
+            var result1 = func1();
+            if (result1)
+            {
+                var result2 = func2(result1);
+                if (result2)
+                {
+                    var result3 = func3(result2);
+                    if (result3)
+                    {
+                        response = response.WithValue(funcResult(result3));
+                    }
+                }
+            }
+
+            return response;
+        }
     }
 }
