baa8851e630742fd5d9f83b194368de5f658f538
Matthew Dove <matthew.dove@blackcombsoftware.com>
2016-04-06 22:18:18 +1000

    Added another layer of compose functions.

--- ContainerExpressions/ContainerExpressions/Expressions/Core/Compose.cs
+++ ContainerExpressions/ContainerExpressions/Expressions/Core/Compose.cs
@@ -6,25 +6,28 @@ namespace ContainerExpressions.Expressions.Core
 {
     internal static class Compose
     {
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Response<TResult> Evaluate<T1, TResult>(Func<Response<T1>> func1, Func<T1, Response<TResult>> funcResult)
         {
             var result = func1();
             return result ? funcResult(result) : Response.Create<TResult>();
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Response<TResult> Evaluate<T1, T2, TResult>(Func<Response<T1>> func1, Func<T1, Response<T2>> func2, Func<T2, Response<TResult>> funcResult)
         {
             var result = func1();
             return result ? Evaluate(() => func2(result), funcResult) : Response.Create<TResult>();
         }
 
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Response<TResult> Evaluate<T1, T2, T3, TResult>(Func<Response<T1>> func1, Func<T1, Response<T2>> func2, Func<T2, Response<T3>> func3, Func<T3, Response<TResult>> funcResult)
         {
             var result = func1();
             return result ? Evaluate(() => func2(result), func3, funcResult) : Response.Create<TResult>();
+        }
+
+        public static Response<TResult> Evaluate<T1, T2, T3, T4, TResult>(Func<Response<T1>> func1, Func<T1, Response<T2>> func2, Func<T2, Response<T3>> func3, Func<T3, Response<T4>> func4, Func<T4, Response<TResult>> funcResult)
+        {
+            var result = func1();
+            return result ? Evaluate(() => func2(result), func3, func4, funcResult) : Response.Create<TResult>();
         }
     }
 }
