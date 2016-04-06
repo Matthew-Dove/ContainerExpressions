b231bd0d2dc5a9c5031e376829f8dc50d40da548
Matthew Dove <matthew.dove@blackcombsoftware.com>
2016-04-07 00:01:25 +1000

    Added regions to seperate the compose response types out.

--- ContainerExpressions/ContainerExpressions/Expressions/Core/Compose.cs
+++ ContainerExpressions/ContainerExpressions/Expressions/Core/Compose.cs
@@ -5,16 +5,12 @@ namespace ContainerExpressions.Expressions.Core
 {
     internal static class Compose
     {
+        #region Response
+
         public static Response Evaluate<T1>(Func<Response<T1>> func1, Func<T1, Response> funcResult)
         {
             var result = func1();
             return new Response(result && funcResult(result));
-        }
-
-        public static Response<TResult> Evaluate<T1, TResult>(Func<Response<T1>> func1, Func<T1, Response<TResult>> funcResult)
-        {
-            var result = func1();
-            return result ? funcResult(result) : Response.Create<TResult>();
         }
 
         public static Response Evaluate<T1, T2>(Func<Response<T1>> func1, Func<T1, Response<T2>> func2, Func<T2, Response> funcResult)
@@ -23,22 +19,10 @@ namespace ContainerExpressions.Expressions.Core
             return result ? Evaluate(() => func2(result), funcResult) : new Response();
         }
 
-        public static Response<TResult> Evaluate<T1, T2, TResult>(Func<Response<T1>> func1, Func<T1, Response<T2>> func2, Func<T2, Response<TResult>> funcResult)
-        {
-            var result = func1();
-            return result ? Evaluate(() => func2(result), funcResult) : Response.Create<TResult>();
-        }
-
         public static Response Evaluate<T1, T2, T3>(Func<Response<T1>> func1, Func<T1, Response<T2>> func2, Func<T2, Response<T3>> func3, Func<T3, Response> funcResult)
         {
             var result = func1();
             return result ? Evaluate(() => func2(result), func3, funcResult) : new Response();
-        }
-
-        public static Response<TResult> Evaluate<T1, T2, T3, TResult>(Func<Response<T1>> func1, Func<T1, Response<T2>> func2, Func<T2, Response<T3>> func3, Func<T3, Response<TResult>> funcResult)
-        {
-            var result = func1();
-            return result ? Evaluate(() => func2(result), func3, funcResult) : Response.Create<TResult>();
         }
 
         public static Response Evaluate<T1, T2, T3, T4>(Func<Response<T1>> func1, Func<T1, Response<T2>> func2, Func<T2, Response<T3>> func3, Func<T3, Response<T4>> func4, Func<T4, Response> funcResult)
@@ -47,10 +31,34 @@ namespace ContainerExpressions.Expressions.Core
             return result ? Evaluate(() => func2(result), func3, func4, funcResult) : new Response();
         }
 
+        #endregion
+
+        #region ResponseT
+
+        public static Response<TResult> Evaluate<T1, TResult>(Func<Response<T1>> func1, Func<T1, Response<TResult>> funcResult)
+        {
+            var result = func1();
+            return result ? funcResult(result) : Response.Create<TResult>();
+        }
+
+        public static Response<TResult> Evaluate<T1, T2, TResult>(Func<Response<T1>> func1, Func<T1, Response<T2>> func2, Func<T2, Response<TResult>> funcResult)
+        {
+            var result = func1();
+            return result ? Evaluate(() => func2(result), funcResult) : Response.Create<TResult>();
+        }
+
+        public static Response<TResult> Evaluate<T1, T2, T3, TResult>(Func<Response<T1>> func1, Func<T1, Response<T2>> func2, Func<T2, Response<T3>> func3, Func<T3, Response<TResult>> funcResult)
+        {
+            var result = func1();
+            return result ? Evaluate(() => func2(result), func3, funcResult) : Response.Create<TResult>();
+        }
+
         public static Response<TResult> Evaluate<T1, T2, T3, T4, TResult>(Func<Response<T1>> func1, Func<T1, Response<T2>> func2, Func<T2, Response<T3>> func3, Func<T3, Response<T4>> func4, Func<T4, Response<TResult>> funcResult)
         {
             var result = func1();
             return result ? Evaluate(() => func2(result), func3, func4, funcResult) : Response.Create<TResult>();
         }
+
+        #endregion
     }
 }
