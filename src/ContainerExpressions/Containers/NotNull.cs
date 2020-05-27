d0d14ce040866c0b451ceda17a85f320e59d5cc8
Matthew Dove <mymailenator+github@gmail.com>
2020-05-28 06:16:16 +1000

    Added NotNull<T> class, and implemented IEquatable<T>.

---
+++ src/ContainerExpressions/Containers/NotNull.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ContainerExpressions.Containers
+{
+    public sealed class NotNull<T> : IEquatable<NotNull<T>> where T : class
+    {
+        public T Value { get; }
+
+        private NotNull(T value)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
+            Value = value;
+        }
+
+        public static implicit operator T(NotNull<T> notNull) => notNull.Value;
+
+        public static implicit operator NotNull<T>(T value) => new NotNull<T>(value);
+
+        public bool Equals(NotNull<T> other) => other != null && other.Value.Equals(Value);
+
+        public override bool Equals(object obj) => Equals(obj as NotNull<T>);
+
+        public override int GetHashCode() => Value.GetHashCode();
+
+        public override string ToString() => Value.ToString();
+
+        public static bool operator !=(NotNull<T> x, NotNull<T> y) => !(x == y);
+
+        public static bool operator ==(NotNull<T> x, NotNull<T> y)
+        {
+            if (x == null && y == null) return true;
+            if (x == null || y == null) return false;
+            return x.Value.Equals(y.Value);
+        }
+    }
+}
