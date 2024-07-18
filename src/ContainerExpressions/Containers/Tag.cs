7728966a1f861f83c9b10c09fba76f6378b3b7f4
Matthew Dove <mymailenator+github@gmail.com>
2024-07-19 02:11:57 +1000

    Renamed Message, to Tag.

---
+++ src/ContainerExpressions/Containers/Tag.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace ContainerExpressions.Containers
+{
+    public static class TagExtensions
+    {
+        public static Tag<TReference> Tag<TReference>(this TReference reference)
+        {
+            return new Tag<TReference>(reference);
+        }
+
+        public static TagReference<TReference> TagReference<TReference>(this TReference reference) where TReference : class
+        {
+            return new TagReference<TReference>(reference);
+        }
+    }
+
+    public readonly struct Tag<TReference>
+    {
+        private readonly TReference _reference;
+
+        public Tag(TReference reference)
+        {
+            _reference = reference;
+        }
+
+        public TReference Set<TMessage>(TMessage message)
+        {
+            TagBase.Set(_reference, message);
+            return _reference;
+        }
+
+        public Response<TMessage> Get<TMessage>()
+        {
+            return TagBase.Get(_reference, default(TMessage));
+        }
+    }
+
+    public readonly struct TagReference<TReference> where TReference : class
+    {
+        private readonly TReference _reference;
+
+        public TagReference(TReference reference)
+        {
+            _reference = reference;
+        }
+
+        public TReference Set<TMessage>(TMessage message)
+        {
+            TagBase.SetReference(_reference, message);
+            return _reference;
+        }
+
+        public Response<TMessage> Get<TMessage>()
+        {
+            return TagBase.GetReference(_reference, default(TMessage));
+        }
+    }
+
+    internal sealed class TagCache<TReference, TMessage>
+    {
+        private static readonly ConcurrentDictionary<TReference, TMessage> _dic = new();
+
+        public static void Set(TReference reference, TMessage message)
+        {
+            _dic.TryAdd(reference, message);
+        }
+
+        public static Response<TMessage> Get(TReference reference)
+        {
+            var result = _dic.TryRemove(reference, out TMessage message);
+            return result ? Response.Create(message) : Response<TMessage>.Error;
+        }
+    }
+
+    internal abstract class TagBase
+    {
+        public static void Set<TReference, TMessage>(TReference reference, TMessage message)
+        {
+            TagCache<TReference, TMessage>.Set(reference, message);
+        }
+
+        public static Response<TMessage> Get<TReference, TMessage>(TReference reference, TMessage message)
+        {
+            return TagCache<TReference, TMessage>.Get(reference);
+        }
+
+        private static long GetUnsafeReference<TReference>(TReference reference) where TReference : class
+        {
+            long address;
+
+            unsafe
+            {
+#pragma warning disable CS8500
+                TypedReference tr = __makeref(reference);
+                IntPtr ptr = **(IntPtr**)(&tr);
+                address = ptr.ToInt64();
+#pragma warning restore CS8500
+            }
+
+            return address;
+        }
+
+        public static void SetReference<TReference, TMessage>(TReference reference, TMessage message) where TReference : class
+        {
+            var address = GetUnsafeReference(reference);
+            TagCache<ValueAlias<long>, TMessage>.Set(ValueAlias.Create(address), message);
+        }
+
+        public static Response<TMessage> GetReference<TReference, TMessage>(TReference reference, TMessage message) where TReference : class
+        {
+            var address = GetUnsafeReference(reference);
+            return TagCache<ValueAlias<long>, TMessage>.Get(ValueAlias.Create(address));
+        }
+    }
+}
---
+++ src/Tests.ContainerExpressions/Containers/TagTests.cs
@@ -0,0 +1,96 @@
+﻿using ContainerExpressions.Containers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Tests.ContainerExpressions.Containers
+{
+    [TestClass]
+    public class TagTests
+    {
+        [TestMethod]
+        public void Tag_Set_Get()
+        {
+            var message = 42;
+
+            var reference = Guid.NewGuid().Tag().Set(message);
+
+            Assert.AreEqual(message, reference.Tag().Get<int>());
+        }
+
+        [TestMethod]
+        public void Tag_Set_Get_Get()
+        {
+            var message = 42;
+
+            var reference = Guid.NewGuid().Tag().Set(message);
+
+            Assert.AreEqual(message, reference.Tag().Get<int>());
+            Assert.IsFalse(reference.Tag().Get<int>());
+        }
+
+        [TestMethod]
+        public void Tag_Get()
+        {
+            var reference = Guid.NewGuid();
+
+            Assert.IsFalse(reference.Tag().Get<int>());
+        }
+
+        [TestMethod]
+        public void TagReference_Set_Get_String()
+        {
+            var message = 42;
+
+            var reference = "hello world".TagReference().Set(message);
+
+            Assert.IsFalse("Hello World!".TagReference().Get<int>());
+            Assert.AreEqual(message, reference.TagReference().Get<int>());
+        }
+
+        [TestMethod]
+        public void TagReference_Set_Get_Object()
+        {
+            var message = 42;
+
+            var reference = new object().TagReference().Set(message);
+
+            Assert.IsFalse(new object().TagReference().Get<int>());
+            Assert.AreEqual(message, reference.TagReference().Get<int>());
+        }
+
+        public sealed class MyClass1
+        {
+            public string MyProperty1 { get; set; }
+            public object MyProperty2 { get; set; }
+            public MyClass2 MyProperty3 { get; set; }
+
+            public MyClass1()
+            {
+                MyProperty1 = "hey chum";
+                MyProperty2 = 1337;
+                MyProperty3 = new MyClass2();
+            }
+        }
+
+        public class MyClass2
+        {
+            public Guid MyProperty { get; set; }
+
+            public MyClass2()
+            {
+                MyProperty = Guid.NewGuid();
+            }
+        }
+
+        [TestMethod]
+        public void TagReference_Set_Get_MyClass()
+        {
+            var message = new MyClass1();
+
+            var reference = message.TagReference().Set(message);
+
+            Assert.IsFalse(new MyClass1().TagReference().Get<MyClass1>());
+            Assert.ReferenceEquals(message.MyProperty2, reference.TagReference().Get<MyClass1>().Value.MyProperty2);
+        }
+    }
+}
